85b8bcd73774644f8c797f04a5b43dfcccc344ba
WHamham <31476244+WHamham@users.noreply.github.com>
2020-10-24 02:28:10 +0200

    test

--- SampleFunctionapp/Function1.cs
+++ SampleFunctionapp/Function1.cs
@@ -24,7 +24,7 @@ namespace SampleFunctionapp
 
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+                : new BadRequestObjectResult("Please spass a name on the query string or in the request body");
         }
     }
 }
