e6a958739ad780620a1aa93342fd92b604d4509f
WHamham <31476244+WHamham@users.noreply.github.com>
2020-10-24 02:41:33 +0200

    Test2

--- SampleFunctionapp/Function1.cs
+++ SampleFunctionapp/Function1.cs
@@ -1,30 +1,35 @@
 
 using System.IO;
+using System.Threading;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
 
 namespace SampleFunctionapp
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static async System.Threading.Tasks.Task<IActionResult> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
 
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please spass a name on the query string or in the request body");
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed sucessfully"
+                : $"Hello, {name}. This HTTP triggered function executed succesfully.";
+
+            return new OkObjectResult(responseMessage);
         }
     }
 }
