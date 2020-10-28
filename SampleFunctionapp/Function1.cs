b087a6bbef3b800cf60aa9443d9d350f3ce6f5c6
WHamham <31476244+WHamham@users.noreply.github.com>
2020-10-28 20:36:12 +0100

    tr

--- SampleFunctionapp/Function1.cs
+++ SampleFunctionapp/Function1.cs
@@ -26,7 +26,7 @@ namespace SampleFunctionapp
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed sucessfully"
+                ? "This HTTP triggered function executed sucessfully !!!!!!"
                 : $"Hellossss, {name}. This HTTP triggered function executed succesfully.";
 
             return new OkObjectResult(responseMessage);
